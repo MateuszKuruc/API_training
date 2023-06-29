3dfb5c0a2c999625ed2203172ba209504409c15c
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-06-29 20:08:27 +0200

    add random img button, add search button based on user input

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const img = document.querySelector('img');
+const userInput = document.querySelector('#userInput');
+const searchButton = document.querySelector('#searchButton');
+const randomButton = document.querySelector('#randomImg');
+let searchWord = 'random';
 
 function getImage() {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=e3c77Afvx3ycZsE6EQqAs0nlZL0MoybC&s=cats', {mode: 'cors'})
+   
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=e3c77Afvx3ycZsE6EQqAs0nlZL0MoybC&s=${searchWord}`, {mode: 'cors'})
 .then(function(response) {
     return response.json();
 })
@@ -12,14 +17,18 @@ function getImage() {
     console.log(response.data.images.original.url);
     img.src = response.data.images.original.url;
     
-});
+}).catch (function(error) {
+    console.log(error);
+    alert('No image found, try again!');
+})
 }
-
-const searchButton
-
-const searchWord = 
 
 window.onload = getImage();
 
-const button = document.getElementById('newImg');
-button.addEventListener('click', getImage);
+randomButton.addEventListener('click', getImage);
+
+searchButton.addEventListener('click', () => {
+    searchWord = userInput.value;
+    userInput.value = '';
+    getImage();
+})
