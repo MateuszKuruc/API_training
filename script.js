747e69e112c864b4c812435c41e424264594b8a9
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-06-29 21:36:26 +0200

    add another version with async and await words

--- script.js
+++ script.js
@@ -6,29 +6,42 @@ const searchButton = document.querySelector('#searchButton');
 const randomButton = document.querySelector('#randomImg');
 let searchWord = 'random';
 
-function getImage() {
+// version 1: Promises
+
+// function getImage() {
    
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=e3c77Afvx3ycZsE6EQqAs0nlZL0MoybC&s=${searchWord}`, {mode: 'cors'})
-.then(function(response) {
-    return response.json();
-})
-.then(function(response) {
-    console.log(response);
-    console.log(response.data.images.original.url);
-    img.src = response.data.images.original.url;
+//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=e3c77Afvx3ycZsE6EQqAs0nlZL0MoybC&s=${searchWord}`, {mode: 'cors'})
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(function(response) {
+//     console.log(response);
+//     console.log(response.data.images.original.url);
+//     img.src = response.data.images.original.url;
     
-}).catch (function(error) {
-    console.log(error);
-    alert('No image found, try again!');
-})
-}
+// }).catch (function(error) {
+//     console.log(error);
+//     alert('No image found, try again!');
+// })
+// }
 
-window.onload = getImage();
-
-randomButton.addEventListener('click', getImage);
+randomButton.addEventListener('click', getImages);
 
 searchButton.addEventListener('click', () => {
     searchWord = userInput.value;
     userInput.value = '';
-    getImage();
+    getImages();
 })
+
+window.onload = getImages();
+
+
+// Version 2: async and await
+// rewrite using async version. 
+// Async doesn't work in global scope, use function block
+
+async function getImages() {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=e3c77Afvx3ycZsE6EQqAs0nlZL0MoybC&s=${searchWord}`, {mode: 'cors'});
+    const imageData = await response.json();
+    img.src = imageData.data.images.original.url;
+};
